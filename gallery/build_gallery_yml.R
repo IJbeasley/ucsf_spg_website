f0783b5e72e1bf6b291568c414058f67882037ec
IJbeasley <isobel.beasley@ucsf.edu>
2025-08-12 20:49:44 -0700

    Set up gallery from spreadsheet

--- gallery/build_gallery_yml.R
+++ gallery/build_gallery_yml.R
@@ -1,31 +1,74 @@
 library(yaml)
 
-# Set folder and output
-image_folder <- here::here("images")
-output_file <- here::here("gallery/gallery.yml")
+
+output_file = here::here("gallery/gallery.yml")
+
+{
+  googlesheets4::gs4_auth(token = gargle::secret_read_rds(
+    ".secrets/gs4-token.rds",
+    key = "GARGLE_OAUTH_KEY"
+  ))
+  
+  gallery_sheet_url <- Sys.getenv("GALLERY_GSHEET_URL")
+  gallery_spreadsheet <- googlesheets4::read_sheet(gallery_sheet_url)
+  
+}
 
 
-# List image files
-image_files <- list.files(image_folder, 
-                          pattern = "\\.(jpg|jpeg|png|webp|svg)$", 
-                          ignore.case = TRUE
-                          )
-# Where to find images, relative to .yml output
-image_folder <- fs::path_rel(path = image_folder,
-                             start = here::here("gallery")
-                             )
+# Functions I custom built to process columns etc. are found in this
+# Rscript
+source(here::here("events/fns_clean_spreadsheet.R"))
 
-# Create YAML-friendly list
-gallery <- lapply(seq_along(image_files), function(i) {
-  file <- image_files[i]
-  list(
-    title = paste("SPG Test Photo #", i),
-    quarter = "Fall 2021",
-    description = "Test Description",
-    alt_text = "Test Alt Text",
-    path = paste0(image_folder, "/", file)
+names(gallery_spreadsheet) <- clean_names(
+  names(gallery_spreadsheet)
+)
+
+{
+  col_title <- "title"
+  col_file_link <- "file_link"
+  col_file_name <- "file_name"
+  col_description <- "description"
+  col_alt_text <- "alt_text"
+  col_date <- "date"
+}
+
+
+##################### Process spreadsheet gallery files ###############
+# Loop through rows (each event) to generate .qmd files
+# Build gallery list from spreadsheet
+gallery <- vector("list", nrow(gallery_spreadsheet))
+
+for (i in 1:nrow(gallery_spreadsheet)) {
+  
+  title <- stringr::str_trim(gallery_spreadsheet[[col_title]][i])
+  
+  quarter <- stringr::str_trim(gallery_spreadsheet[[col_date]][i])
+  
+  description <- stringr::str_trim(gallery_spreadsheet[[col_description]][i])
+  
+  alt_text <- stringr::str_trim(gallery_spreadsheet[[col_alt_text]][i])
+  
+  photo_link <- stringr::str_trim(gallery_spreadsheet[[col_file_link]][i])
+  photo_name <- stringr::str_trim(gallery_spreadsheet[[col_file_name]][i])
+  
+  if(!file.exists(paste0("images/", photo_name))){
+
+    googledrive::drive_download(photo_link,
+                                path = paste0("images/", photo_name))
+    
+  }
+  
+  photo_path <- paste0("../images/", photo_name)
+  
+  # Add to YAML list
+  gallery[[i]] <- list(
+    title       = title,
+    quarter     = quarter,
+    description = description,
+    alt_text    = alt_text,
+    path        = photo_path
   )
-})
-
-# Write to YAML
-writeLines(as.yaml(gallery), output_file)
+}
+  
+# Write YAML
+writeLines(as.yaml(gallery), output_file)  
